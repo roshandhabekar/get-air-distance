389be594088a5896c0fb9f5a3f871c53b85896da
roshan <roshan_dhabekar@persistent.com>
2023-12-11 16:10:22 +0530

    removed unused code

--- index.js
+++ index.js
@@ -9,10 +9,8 @@
  * @param {double} long1 - longitude of first location
  * @param {double} lat2- latitude of second location
  * @param {double} long2 - longitude of second location
- * @return {double} distance - description
  */
 function calculate(lat1, long1, lat2, long2) {
-    const distance = 30;
     let y = lat2 - lat1;
     let x = long2 - long1;
     return Math.sqrt(x * x + y * y);
